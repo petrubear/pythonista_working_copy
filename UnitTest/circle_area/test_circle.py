82ea42f927be2acdb96bdbd557f23ce12e35ccdb
Edison Martínez <petrubear@users.noreply.github.com>
2017-12-30 00:40:07 -0500

    initial import
    Ipad test commit

---
+++ UnitTest/circle_area/test_circle.py
@@ -0,0 +1,22 @@
+import unittest
+from math import pi
+from circle import area
+
+
+class TestCircleArea(unittest.TestCase):
+    def test_area(self):
+        # Test areas when radius >= 0
+        self.assertAlmostEqual(area(1), pi)
+        self.assertAlmostEqual(area(0), 0)
+        self.assertAlmostEqual(area(2.1), pi * 2.1**2)
+
+    def test_values(self):
+        #Make sure value errors are raised
+        self.assertRaises(ValueError, area, -2)
+
+    def test_types(self):
+        # Make sure type errors are raised when necessary
+        self.assertRaises(TypeError, area, 3 + 5j)
+        self.assertRaises(TypeError, area, True)
+        self.assertRaises(TypeError, area, "radius")
+        
