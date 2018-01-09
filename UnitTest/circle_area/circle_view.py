9f68e379e7980f81c8a55ea5435eba0e1a6774e5
Edison Martínez <petrubear@users.noreply.github.com>
2018-01-08 19:14:29 -0500

    add errors on alert

--- UnitTest/circle_area/circle_view.py
+++ UnitTest/circle_area/circle_view.py
@@ -1,11 +1,16 @@
 import ui
+import console
 from circle import area
 
+@ui.in_background
 def btn_area_onclick(sender):
   view = sender.superview
   tarea = view['txt_area']
   tradius = view['txt_radius']
-  tarea.text = '{:f}'.format(area(float(tradius.text)))
+  try:
+      tarea.text = '{:f}'.format(area(float(tradius.text)))
+  except Exception as err:
+      console.alert('Error', str(err))
     
     
 v = ui.load_view()
