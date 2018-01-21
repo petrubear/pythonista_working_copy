d35ceb86dee75b14c90e99992b39066333c765ee
Edison Martínez <petrubear@users.noreply.github.com>
2018-01-20 21:16:45 -0500

    style: code format

--- UnitTest/circle_area/circle_view.py
+++ UnitTest/circle_area/circle_view.py
@@ -2,17 +2,18 @@ import ui
 import console
 from circle import area
 
+
 @ui.in_background
 def btn_area_onclick(sender):
   view = sender.superview
   tarea = view['txt_area']
   tradius = view['txt_radius']
   try:
-      tarea.text = '{:f}'.format(area(float(tradius.text)))
+    tarea.text = '{:f}'.format(area(float(tradius.text)))
   except Exception as err:
-      console.alert('Error', str(err))
-    
-    
+    console.alert('Error', str(err))
+
+
 v = ui.load_view()
 v.present('sheet')
 
